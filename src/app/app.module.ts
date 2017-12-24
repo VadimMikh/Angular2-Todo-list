54e06a76cc58b933b870e2aaa4bf83c81d2b621e
VadimMikh <vadossm82@gmail.com>
2017-12-24 17:16:21 +0100

    Sintax fix

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TodoInputComponent } from './todo-input/todo-input.component';
-import {FormsModule} from '@angular/forms';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
