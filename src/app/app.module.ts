c00a10d8b49cccf6594a66917e400f5263daeff8
VadimMikh <vadossm82@gmail.com>
2017-12-24 17:11:51 +0100

    Add input component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
+import { TodoInputComponent } from './todo-input/todo-input.component';
+import {FormsModule} from '@angular/forms';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodoInputComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/todo-input/todo-input.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'todo-input',
+  templateUrl: './todo-input.component.html',
+  styleUrls: ['./todo-input.component.css']
+})
+export class TodoInputComponent {
+
+  newtask:string = '';
+
+  constructor() {
+
+  }
+}
